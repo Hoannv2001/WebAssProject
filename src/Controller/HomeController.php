f2a4b078f397b6bc160716f0be81252d333b18ff
manhnguyen <thaimanh2108@gmail.com>
2022-10-28 16:31:18 +0700

    home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,14 +13,50 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(BookRepository $bookRepository): Response
+    public function index(BookRepository $bookRepository, AuthorRepository $authorRepository,
+                          CategoryRepository $categoryRepository, LoggerInterface $logger): Response
     {
-        $temQuery = $bookRepository->countOfBook();
+        $user=$this->getUser();
+        $hasAccess = $this->isGranted('ROLE_SELLER');
+        $hasAccessCus = $this->isGranted('ROLE_CUSTOMER');
+        $hasAccessAdmin = $this->isGranted('ROLE_ADMIN');
+        $temQuery = $bookRepository->selectDataBookAdmin($user);
         $paginator = new Paginator($temQuery);
         $totalItems = count($paginator);
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'countOfBook' =>$totalItems
-        ]);
+
+        $temQueryBook = $bookRepository->countOfBook();
+        $paginator = new Paginator($temQueryBook);
+        $totalItemsBook = count($paginator);
+        $temQuery1 = $authorRepository->countOfAuthor();
+        $paginator = new Paginator($temQuery1);
+        $totalItems1 = count($paginator);
+
+        $temQuery2 = $categoryRepository->countOfCategory();
+        $paginator = new Paginator($temQuery2);
+        $totalItems2 = count($paginator);
+
+
+        if ($hasAccess){
+            return $this->render('home/index.html.twig',[
+                'controller_name' => 'HomeController',
+                'countOfBook' =>$totalItems,
+                'countOfAuthor'=>$totalItems1,
+                'countOfCategory'=>$totalItems2,
+            ]);
+        }elseif ($hasAccessCus){
+            return $this->render('home/homeCus.html.twig',[
+//                'controller_name' => 'HomeController',
+            ]);
+        }elseif ($hasAccessAdmin){
+            return $this->render('home/index.html.twig',[
+                'controller_name' => 'HomeController',
+                'countOfBook' => $totalItemsBook,
+                'countOfAuthor' => $totalItems1,
+                'countOfCategory' => $totalItems2
+            ]);
+        }
+        else {
+            return $this->render('home/homeCus.html.twig', []);
+        }
     }
 }
